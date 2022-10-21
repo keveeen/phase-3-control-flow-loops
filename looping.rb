39f32a3a39e98888b5520dd0f85cc35fb7959489
keveeen <105822487+keveeen@users.noreply.github.com>
2022-10-21 23:00:28 +0200

    commit tree 67d1c5919d9e0b0e0f4c33e44458a09529b63b14

--- looping.rb
+++ looping.rb
@@ -1,6 +1,43 @@
 def happy_new_year
   # your code here
+
+  counter=10
+  while counter > 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
+
 end
+
+def reverse_string(str)
+  # your code here
+  reversed_string = ""
+  i = 0
+  while i < str.length
+    reversed_string = str[i] + reversed_string
+    i += 1
+  end
+  
+  reversed_string
+end
+
+# i = 0
+# while i < 5
+#   puts "Looping!"
+#   i += 1
+# end
+
+# function reverseString(str) {
+#   let reversedStr = "";
+#   for (let i = 0; i < str.length; i++) {
+#     reversedStr = str[i] + reversedStr;
+#   }
+#   return reversedStr;
+# }
+
+
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -17,8 +54,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  num = 1
+
+  while num <=100
+    puts fizzbuzz(num)
+    num += 1
+  end
 end
 
-def reverse_string(str)
-  # your code here
-end
+
+
+# function fizzbuzzPrinter() {
+#   for (let num = 1; num <= 100; num++) {
+#     console.log(fizzbuzz(num));
+#   }
+# }
